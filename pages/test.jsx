f4c83364e36f9adc167b44a0d466471110ed24ec
XcannesOlivier <desruelleolivier@gmail.com>
2025-04-16 15:33:40 +0200

    Fix test.jsx pour utiliser BinanceStyleChart

--- pages/test.jsx
+++ pages/test.jsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 
-const XrplLiveChart = dynamic(() => import("../components/XrplLiveChart"), {
+// ⚠️ Utilise maintenant le nouveau composant !
+const BinanceStyleChart = dynamic(() => import("../components/BinanceStyleChart"), {
   ssr: false,
 });
 
@@ -8,8 +9,7 @@ export default function TestPage() {
   return (
     <div className="p-6 min-h-screen bg-black text-white">
       <h1 className="text-2xl mb-4">🔥 Test Chart Live - XRP/RLUSD</h1>
-      <XrplLiveChart />
+      <BinanceStyleChart pair="XRP/RLUSD" />
     </div>
   );
 }
-
