32e55f7103d643a4e7f3d68f49ae86225711b3af
XcannesOlivier <desruelleolivier@gmail.com>
2025-04-16 14:29:52 +0200

    Intégration WebSocket dynamique pour toutes les paires dans XrplLiveChart

--- utils/xrpl.js
+++ utils/xrpl.js
@@ -12,11 +12,11 @@ export const getBookIdFromPair = (pair) => {
         taker_gets: { currency: "XCS", issuer: "rBxQY3dc4mJtcDA5UgmLvtKsdc7vmCGgxx" },
         taker_pays: { currency: "EUR", issuer: "rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq" },
       },
-      "XCS/RLUSD": {
+      "XRP/RLUSD": {
         taker_gets: { currency: "XCS", issuer: "rBxQY3dc4mJtcDA5UgmLvtKsdc7vmCGgxx" },
         taker_pays: { currency: "524C555344000000000000000000000000000000", issuer: "rMxCKbEDwqr76QuheSUMdEGf4B9xJ8m5De" },
       },
-      "XRP/RLUSD": {
+      "XCS/RLUSD": {
         taker_gets: { currency: "XRP" },
         taker_pays: { currency: "524C555344000000000000000000000000000000", issuer: "rMxCKbEDwqr76QuheSUMdEGf4B9xJ8m5De" },
       },
