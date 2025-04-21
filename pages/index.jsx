27b65aa510f446c4df50ef7f00355f4566ae0219
XcannesOlivier <desruelleolivier@gmail.com>
2025-04-21 21:09:37 +0200

    Fix import/export issue for useMarketData

--- pages/index.jsx
+++ pages/index.jsx
@@ -31,7 +31,7 @@ export default function Home() {
     <img
       src="/assets/img/ui/xcannesgreen.png"
       alt="Logo X"
-      className="h-[10.2rem] sm:h-[16.2rem] md:h-[19rem] w-auto max-w-full brightness-110 saturate-150 pt-[3rem] sm:pt-0 md:pt-0 mt-0 sm:mt-0 md:mt-0"
+      className="h-[11.2rem] sm:h-[16.2rem] md:h-[19rem] w-auto max-w-full brightness-110 saturate-150 pt-[3rem] sm:pt-0 md:pt-0 mt-0 sm:mt-0 md:mt-0"
     />
   </span>
 </h1>
