00cc5571dd4fe2821ac6412f5315746dc64c6de3
XcannesOlivier <desruelleolivier@gmail.com>
2025-04-25 09:13:55 +0200

    Fix import/export issue for useMarketData

--- components/TokenDistributionChart.jsx
+++ components/TokenDistributionChart.jsx
@@ -132,8 +132,8 @@ export default function TokenDistributionChart() {
                   data={data}
                   cx="50%"
                   cy="50%"
-                  innerRadius={isMobile ? 80 : 80}
-                  outerRadius={isMobile ? 120 : 120}
+                  innerRadius={isMobile ? 60 : 80}
+                  outerRadius={isMobile ? 100 : 120}
                   dataKey="value"
                   label={renderCustomLabel}
                   labelLine={false}
